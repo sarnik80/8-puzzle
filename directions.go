bf3cc37e7c34d99066680541484dc05a32351a0a
sarnik80 <sarah80nik@gmail.com>
2023-05-29 11:19:32 +0330

    added direction file to define all 4 directions [Up,Down,Right, Left]

---
+++ directions.go
@@ -0,0 +1,56 @@
+package main
+
+import "strconv"
+
+// Here we simulate C’s enum by Go’s iota for all direction
+
+type enumDirection uint8
+
+const (
+	Up = enumDirection(iota)
+	Down
+	Left
+	Right
+)
+
+// lable
+
+func (d enumDirection) String() string {
+	name := []string{"up", "down", "left", "right"}
+	i := uint8(d)
+	switch {
+	case i <= uint8(Right):
+		return name[i]
+	default:
+		return strconv.Itoa(int(i))
+	}
+}
+
+type Direction struct {
+	Lable enumDirection
+
+	X, Y int
+}
+
+/*
+
+	all directions :
+
+	 UP   (-1 , 0)
+	 Down (+1, 0)
+	 Left (0, -1)
+	 Right (0, +1)
+
+*/
+
+func defineAllDirections() *[]Direction {
+
+	u := Direction{X: nOne, Y: zero, Lable: Up}
+	d := Direction{X: one, Y: zero, Lable: Down}
+	l := Direction{X: zero, Y: nOne, Lable: Left}
+	r := Direction{X: zero, Y: one, Lable: Right}
+
+	directions := []Direction{u, d, l, r}
+
+	return &directions
+}
