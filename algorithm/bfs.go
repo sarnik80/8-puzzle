21b08130e1aff98462bccdae79cf79c16928cc55
sarnik80 <sarah80nik@gmail.com>
2023-05-30 17:06:07 +0330

    added some useful comments

--- algorithm/bfs.go
+++ algorithm/bfs.go
@@ -6,10 +6,46 @@ import (
 
 /*
 
+	[bfs] is uninformed search algorithm that used
+	to search a tree or graph data structure .
+
+	it starts at thetree's root of graph and
+	visits all nondes at the current depth
+	level before moving on the nodes at
+	next depth level
+
+
+	to avoid processing one node more than once
+	we divide the nodes into two type :
+
+	1. visited
+	2. not visited
+
+	1. visited is implemented by map[current_node]boolean  data type
+
+	2. BFS uses queue data structure for traversal
+
+
+	** time complextiry  = >   O(V + E)  =>
+
+		V is number of nodes
+		E is number of edges
+
+
+	** space complexity  of bfs  O(b^d)   => b is branching factor  and d is depth
+
+	**if there is a solution , bfs will definitely find it
+	=> is a complete algorithm
+
+
+	** if there is more than one solution bfs can find the
+	minimal one that  requires less  number of steps  => optimal algorithm
 
 
 
- */
+
+
+*/
 
 type BFS struct {
 	Name algorithmName
@@ -22,26 +58,34 @@ func GetName() algorithmName {
 
 func (b BFS) Solve(sourcePuzzle, goalPuzzle string) *eightpuzzle.EghtPuzzle {
 
+	// create source node and source puzzle
+
 	node := &eightpuzzle.Node{Data: sourcePuzzle, Parent: nil, Level: 0}
 
 	source := eightpuzzle.EghtPuzzle{State: node, GoalState: goalPuzzle}
 
 	queue := []*eightpuzzle.EghtPuzzle{&source}
-	visited := map[string]eightpuzzle.EghtPuzzle{sourcePuzzle: source}
+	visited := map[string]bool{sourcePuzzle: true}
 
 	for len(queue) > 0 {
+
+		//  Among all the nodes in the queue, we delete the first node.
 
 		currentPuzzle := queue[0]
 		queue = eightpuzzle.RemoveIndex(queue, 0)
 
+		// End of search
+
 		if currentPuzzle.IsGoal() {
 
-			solution := currentPuzzle
-
-			return solution
+			return currentPuzzle // target node
 		}
 
-		visited[currentPuzzle.State.Data] = *currentPuzzle
+		// The current node is added to the visited nodes
+
+		visited[currentPuzzle.State.Data] = true
+
+		// Extending  the current node
 
 		for _, child := range *currentPuzzle.GetChildren() {
 
