29f786518fa03f4296c26c072993dfbe9dbc07eb
sarnik80 <sarah80nik@gmail.com>
2023-06-04 16:30:01 +0330

    added create function as a constructor for this structure and changed type of G,H,F value from float64 to int32

--- eightPuzzle/node.go
+++ eightPuzzle/node.go
@@ -13,8 +13,22 @@ type Node struct {
 	Data    string
 	Level   int32
 	Dir     Direction
-	G_score float64
-	H_score float64
-	F_score float64
+	G_score int32
+	H_score int32
+	F_score int32
 	Parent  *Node
+}
+
+func CreateNode(data string, level int32, g_score, h_score, f_score int32, parent *Node) *Node {
+
+	newNode := Node{}
+
+	newNode.Data = data
+	newNode.Level = level
+	newNode.G_score = g_score
+	newNode.H_score = h_score
+	newNode.Parent = parent
+
+	return &newNode
+
 }
