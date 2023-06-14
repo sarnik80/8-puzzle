bba6a565312f9346a4701fe9a25d0637d2c39b8e
sarnik80 <sarah80nik@gmail.com>
2023-06-14 08:34:38 +0330

    some const value are written with capital letters

--- eightPuzzle/const.go
+++ eightPuzzle/const.go
@@ -1,10 +1,10 @@
 package eightpuzzle
 
 const (
-	zeroStr = "0"
-	maxRow  = 3
-	maxCol  = 3
-	zero    = 0
-	one     = 1
-	nOne    = -1
+	ZeroStr = "0"
+	MaxRow  = 3
+	MaxCol  = 3
+	Zero    = 0
+	One     = 1
+	NOne    = -1
 )
