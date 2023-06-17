69e154eaff03bcf2110e26d4ce600f7fe747d442
sarnik80 <sarah80nik@gmail.com>
2023-06-17 18:09:19 +0330

    added nodesOfPath function to return nodes of result path

--- eightPuzzle/utils.go
+++ eightPuzzle/utils.go
@@ -83,10 +83,6 @@ func stringedSlice(strSlice []string) string {
 	return strings.Join(strSlice, "")
 }
 
-func RemoveIndex(queue []*EghtPuzzle, index int) []*EghtPuzzle {
-	return append(queue[:index], queue[index+1:]...)
-}
-
 /*
 
  By having the coordinates of a tile in the
@@ -95,14 +91,14 @@ func RemoveIndex(queue []*EghtPuzzle, index int) []*EghtPuzzle {
 
 */
 
-func manhattanDistance(puzzle, goal, strNumber string) float64 {
+func ManhattanDistance(puzzle, goal, strNumber string) int {
 
 	i, j := getCoordinates(puzzle, strNumber)
 	x, y := getCoordinates(goal, strNumber)
 
 	if !(i == x && j == y) {
 
-		return math.Abs(float64(i)-float64(x)) + math.Abs(float64(j)-float64(y))
+		return int(math.Abs(float64(i-x))) + int(math.Abs(float64(j-y)))
 
 	}
 
@@ -129,11 +125,26 @@ func reverse(slc []string) string {
 
 	for i := range slc {
 		// reverse the order
-		rev_slc = append(rev_slc, slc[len(slc)-1-i])
+		if i%3 == 0 {
+			rev_slc = append(rev_slc, slc[len(slc)-1-i])
+		}
 
 	}
 
-	result := strings.Join(rev_slc, "->")
+	result := strings.Join(rev_slc, " -> ")
 
 	return result
+}
+
+func NodesOfPath(solution *Node) string {
+	ph := []string{solution.Data}
+
+	for solution.Parent != nil {
+
+		ph = append(ph, solution.Parent.Data)
+		solution = solution.Parent
+
+	}
+
+	return reverse(ph)
 }
