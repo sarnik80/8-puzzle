40f76560295876d86ff302b7ffba2bad89d1fe2d
sarnik80 <sarah80nik@gmail.com>
2023-06-17 18:03:59 +0330

    create manhattan distance heuristic

---
+++ heuristic/manhattan.go
@@ -0,0 +1,32 @@
+package heuristic
+
+import (
+	"fmt"
+
+	eightpuzzle "github.com/sarnik80/8-puzzle/eightPuzzle"
+)
+
+type Manhattan struct {
+	Name Lable
+}
+
+func (m Manhattan) H_value(puzzle string, goal string) int {
+
+	var total int
+
+	for _, number := range puzzle {
+
+		strNumber := fmt.Sprintf("%c", number)
+
+		total += eightpuzzle.ManhattanDistance(puzzle, goal, strNumber)
+
+	}
+
+	return total
+}
+
+func (m Manhattan) getName() Lable {
+
+	return m.Name
+
+}
